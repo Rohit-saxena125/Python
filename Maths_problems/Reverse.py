edde8971b10d7b892cf07294879e9ade7e626284
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 13:08:31 +0530

    reverse number

---
+++ Maths_problems/Reverse.py
@@ -0,0 +1,10 @@
+#print the reverse number digit using % and //
+num = eval(input("Enter the number:"))
+r1 = num%10
+q1 = num //10
+r2 =q1%10
+q2 =q1//10
+r3 = q2%10
+q3 = q2 //10
+r4 =q3%10
+print("The reverse of ",num,'is:',r1,r2,r3,r4)
