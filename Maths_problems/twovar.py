48feee9296a725cec0c2602fe58e5b36a27d5a68
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 17:02:35 +0530

    calculate expression

---
+++ Maths_problems/twovar.py
@@ -0,0 +1,9 @@
+#calculate expression
+'''
+(2+8p)/2 - (p**2-q**2)/2+ 4 *(p+q)/2
+'''
+p = eval(input("Enter the number:"))
+q = eval(input("Enter the number:"))
+z= (2+8*p)/2 - (p**2-q**2)/2+ 4 *(p+q)/2
+print('(2+8p)/2 - (p**2-q**2)/2+ 4 *(p+q)/2')
+print(z)
