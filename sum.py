73e124d8ba0c5dab7850994ff4aaef37ddb95335
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 14:51:17 +0530

    Delete sum.py

--- sum.py
+++
@@ -1,4 +0,0 @@
-# Find sum of two number entered by user.
-a = int(input("Enter first number :"))
-b = int(input("Enter second number :"))
-print("sum of two number {0} and {1} is:".format(a,b),a+b)
