69728ded58f5c4dc7a221dae861b002b5963c911
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 13:07:50 +0530

    profit

---
+++ Maths_problems/Profit.py
@@ -0,0 +1,4 @@
+# find profit earn to sell a product
+sp = eval(input("Enter the selling price of the product:"))
+cp = eval(input("Enter the cost price of the product:"))
+print("The selling price is",sp,"\nThe cost price is",cp,"\nprofit earned by selling =",sp-cp)
