c95f2d788e44ea35aa3282ddba261df8eadfda9d
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 13:09:33 +0530

    temperature

---
+++ Maths_problems/Temperature.py
@@ -0,0 +1,9 @@
+#find the temperature in degree
+celsius =eval(input("Enter the Degree of celsius:"))
+Farhenite =(9/5)*celsius+32
+print("Temperature in Farhenite=",Farhenite)
+
+print("-------------------------------------------------------------------------------------------------------------")
+Farhenite =eval(input("Enter the Degree of Farhenite:"))
+celsius =(5/9)*(Farhenite-32)
+print("Temperature in celsius=",celsius)
