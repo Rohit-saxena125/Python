7e034e1ed520b5a8eaab1bb7f16caf33916644f3
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 14:50:34 +0530

    Delete pattern2.py

--- pattern2.py
+++
@@ -1,9 +0,0 @@
-k = int(input('enter the number of line:'))
-n=2*k-2
-for i in range (k) :
-    for j in range (n):
-        print(' ')
-    k -=2
-    for j in range(0,i+1):
-        print('*',end='')
-    print() 
