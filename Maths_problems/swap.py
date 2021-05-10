4b514bbb663a48d32e64bb7bdb5231ee2884fd09
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 15:13:34 +0530

    swapping

---
+++ Maths_problems/swap.py
@@ -0,0 +1,7 @@
+# swap two number using third variable.
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c=a
+a=b
+b=c
+print("swap of two number is:{0},{1}".format(a,b))
