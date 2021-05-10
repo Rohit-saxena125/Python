784cfb8a07cc927bc0f0a419b1637a3b2ea9c5cf
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 13:06:05 +0530

    Area of rectangle

---
+++ Maths_problems/Area.py
@@ -0,0 +1,5 @@
+# Find area of rectangle
+length = int(input("Enter the length :"))
+Breadth = int(input("Enter the Breadth :"))
+print("Area of reactangle =",length*Breadth)
+print("Thanks for visiting!!!")
