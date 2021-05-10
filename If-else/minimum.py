193f04e094aaaa936f98cc586d051cb2cb175794
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 17:38:34 +0530

    minimum and maximum

---
+++ If-else/minimum.py
@@ -0,0 +1,5 @@
+# find minimum  and number entered by user
+num1,num2 = eval(input("Enter number:"))
+min= print(num1,'is minimum') if num1<num2 else print(num2,'is minimum')
+print('--------------------------------------------------------------------------------------------------------------')
+max= print(num1,'is maximum') if num1>num2 else print(num2,'is maximum')
