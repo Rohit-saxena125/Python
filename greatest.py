68c5a183346411f288d3e8b95456b055f3ff46d8
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 14:49:44 +0530

    Delete greatest.py

--- greatest.py
+++
@@ -1,8 +0,0 @@
-# find the greatest number
-a,b,c=map(int,input().split())
-if a>b and a>c:
-    print(a,"is greater")
-elif b>a and b>c:
-    print(b,"is greater")
-else:
-    print(c,"is greater")
