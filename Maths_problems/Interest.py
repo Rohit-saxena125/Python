9fb965355aa7a3e56601a51f267b485215da9ad8
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-10 13:07:23 +0530

    Simple interest

---
+++ Maths_problems/Interest.py
@@ -0,0 +1,6 @@
+#find the simple interest
+principle =eval(input("Enter the principle ammount:"))
+rate =eval(input("Enter the rate of interest:"))
+time =eval(input("Enter the period of time:"))
+si=principle*rate*time/100
+print("The simple interest=",si)
