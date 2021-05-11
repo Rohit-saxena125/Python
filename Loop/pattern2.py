8858d83ad7b26ffc90e6b4e8c06f70859cbcee99
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-11 16:12:43 +0530

    Update pattern2.py

--- Loop/pattern2.py
+++ Loop/pattern2.py
@@ -1,9 +1,16 @@
-k = int(input('enter the number of line:'))
-n=2*k-2
-for i in range (k) :
-    for j in range (n):
-        print(' ')
-    k -=2
-    for j in range(0,i+1):
-        print('*',end='')
-    print() 
+'''
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+'''
+n = int(input("Enter the number of lines :"))
+k = n - 1
+for i in range(0,n):
+    for j in range(0,k):
+        print(end=' ')
+    k -=1
+    for i in range(0,i+1):
+        print('* ',end='')
+    print('\r')
