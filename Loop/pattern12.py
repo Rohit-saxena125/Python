6508ee62eda17884d3e4c9819e84efacf81fd18a
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-11 23:07:50 +0530

    pattern

---
+++ Loop/pattern12.py
@@ -0,0 +1,31 @@
+'''
+   * * * * * *
+    * * * * *
+     * * * *
+      * * *
+       * *
+        *
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+   * * * * * *
+'''
+n = int(input("Enter the number of n: "))
+k = n - 2
+for i in range(n, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k += 1
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print()
+k = 2*n - 2
+for i in range(0,n+1):
+    for j in range(0,k):
+        print(end=' ')
+    k -=1
+    for i in range(0,i+1):
+        print('* ',end='')
+    print('\r')
