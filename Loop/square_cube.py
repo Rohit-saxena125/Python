60d6792c218707a2b7b364ccbe3d539f6473922a
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-11 16:16:07 +0530

    square cube

---
+++ Loop/square_cube.py
@@ -0,0 +1,4 @@
+# print the square and cube of given number using for loop
+num = int(input("Enter the number:"))
+for i in range(1,num+1):
+    print('Square of ',i,'=',i**2,'\ncube of   ',i,'=',i**3)
