1c22f400a792289a095c57adf76c37685ea59f3e
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-22 16:44:56 +0530

    Leap_year

---
+++ Maths_problems/Leap_year.py
@@ -0,0 +1,14 @@
+# check year is leap year or not
+def checkYear(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+year = int(input('Enter the years :'))
+print(checkYear(year))
