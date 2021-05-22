b276b1df895f7246b841b61e6b9affd9be4e3800
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2021-05-22 16:45:58 +0530

    count_number

---
+++ String/count_number.py
@@ -0,0 +1,5 @@
+# find the number of vowels , consonants, digits and white space in a string .
+word = input('Enter the string : ')
+vowel , consonants , digits , whitespace = 0,0,0,0
+for i in word :
+    
